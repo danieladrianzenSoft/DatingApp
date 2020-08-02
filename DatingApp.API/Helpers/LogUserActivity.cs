1bf6c608b06139fc0066921ca9ce4db41c22dec6
Daniel Adrianzen <dzen@Daniels-MacBook-Pro-4.local>
2020-08-02 00:26:05 -0400

    before isonline

--- DatingApp.API/Helpers/LogUserActivity.cs
+++ DatingApp.API/Helpers/LogUserActivity.cs
@@ -1,10 +1,33 @@
 ﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace DatingApp.API.Helpers
 {
-    public class LogUserActivity
+    public class LogUserActivity : IAsyncActionFilter
     {
-        public LogUserActivity()
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
+            // variant of this may allow finding if user is logged in.
+            var resultContext = await next();
+
+            var userId = int.Parse(resultContext.HttpContext.User
+                .FindFirst(ClaimTypes.NameIdentifier).Value);
+
+            var repo = resultContext.HttpContext.RequestServices.GetService<IDatingRepository>();
+
+            var user = await repo.GetUser(userId);
+
+            user.LastActive = DateTime.Now;
+
+            await repo.SaveAll();
+
+            
         }
+
     }
 }
